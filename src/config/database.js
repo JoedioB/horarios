3f1ddfef4ac702e5f4056f60187428f3d90d59f7
Joédio Borges Jr <joedio.borges@ifc.edu.br>
2025-07-18 11:54:34 -0300

    add .env to .gitignore

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,21 @@
-require('dotenv').config();
+import mysql from "mysql2";
+import dotenv from 'dotenv'; // Apenas para desenvolvimento local
+dotenv.config(); // Carrega variáveis do.env para desenvolvimento local
 
-module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
-  },
-  test: {
-    // ... config de teste
-  },
-  production: {
-    // ... config de produção
+const con = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+});
+
+con.connect((error) => {
+  if (error) {
+    console.log("❌ Erro de conexão com o banco de dados:", error);
+  } else {
+    console.log("✅ Conectado ao banco de dados MySQL!");
   }
-};
+});
+
+export default con;
